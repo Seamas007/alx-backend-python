c3cf0fd578b513dce6079f9b8c415e9073978bd4
Seamas007 <olufy007@gmail.com>
2023-07-07 19:50:22 +0100

    My commit

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-
+'''Task 8's module.
+'''
 from typing import Callable
 
-def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    def multiplier_function(num: float) -> float:
-        return num * multiplier
-    return multiplier_function
 
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    '''Creates a multiplier function.
+    '''
+    return lambda x: x * multiplier
