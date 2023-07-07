91f9e072c8db359e356873b0ce5d7fb896500f1f
Seamas007 <olufy007@gmail.com>
2023-07-07 19:17:26 +0100

    my commit

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
+'''Task 1's module.
+'''
 
-# Function to concatenate two strings and return the result
+
 def concat(str1: str, str2: str) -> str:
-# Return sum of two strings
+    '''Concatenates two strings.
+    '''
     return str1 + str2
