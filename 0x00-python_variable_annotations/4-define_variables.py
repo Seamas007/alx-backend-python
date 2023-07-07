3894a0203f87fdbb77d93d28d4907aec7c8dc559
Seamas007 <olufy007@gmail.com>
2023-07-07 19:37:59 +0100

    My commit

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+'''Task 4's module.
+'''
 
-# Define and annotate variables
+
 a: int = 1
 pi: float = 3.14
 i_understand_annotations: bool = True
-school: str = "Holberton"
+school: str = 'Holberton'
